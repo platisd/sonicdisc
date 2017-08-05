06ed5326d074a33b32a12cf945081389268b7e07
Dimitris Platis <kurukii88@gmail.com>
2017-08-05 12:50:17 +0300

    Add initial firmware

---
+++ hardware/firmware/SonicDisc.ino
@@ -0,0 +1,11 @@
+#include "SonicSensor.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ hardware/firmware/SonicSensor.h
@@ -0,0 +1,10 @@
+#ifndef SONIC_SENSOR_H
+#define SONIC_SENSOR_H
+
+class SonicSensor {
+public:
+  SonicSensor();
+  
+};
+
+#endif
