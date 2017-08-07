c33b121efa978b4ca25ceb3060994ba4880ee6df
Dimitris Platis <kurukii88@gmail.com>
2017-08-07 13:27:51 +0300

    Add pin numbers (not Arduino-like)

--- hardware/firmware/SonicDisc.ino
+++ hardware/firmware/SonicDisc.ino
@@ -1,5 +1,16 @@
 #include "SonicSensor.h"
 
+SonicSensor sensors[] = {
+    SonicSensor(PC3, PC2), // Ultrasonic_0 on the Sonic Disc
+    SonicSensor(PC1, PC0), // Ultrasonic_1
+    SonicSensor(PB5, PB4), // Ultrasonic_2
+    SonicSensor(PB3, PB2), // Ultrasonic_3
+    SonicSensor(PB0, PB1), // Ultrasonic_4
+    SonicSensor(PD7, PD6), // Ultrasonic_5
+    SonicSensor(PD4, PD5), // Ultrasonic_6
+    SonicSensor(PD3, PD2)  // Ultrasonic_7
+};
+
 /**
    Pin change interrupt for D8 to D13
 */
