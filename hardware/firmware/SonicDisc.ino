0ede513902aad0b13ecd8497e21856ede7d46320
Dimitris Platis <kurukii88@gmail.com>
2017-08-07 13:10:49 +0300

    Setup change interrupt method
    Used example from https://playground.arduino.cc/Main/PinChangeInterrupt

--- hardware/firmware/SonicDisc.ino
+++ hardware/firmware/SonicDisc.ino
@@ -1,8 +1,34 @@
 #include "SonicSensor.h"
 
-void setup() {
-  // put your setup code here, to run once:
+/**
+   Pin change interrupt for D8 to D13
+*/
+ISR (PCINT0_vect) {
 
+}
+
+/**
+   Pin change interrupt for A0 to A5
+*/
+ISR(PCINT1_vect) {
+
+}
+
+/**
+   Pin change interrupt for D0 to D7
+*/
+ISR (PCINT2_vect) {
+
+}
+
+void setupChangeInterrupt(uint8_t pin) {
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
+void setup() {
+    setupChangeInterrupt(1);
 }
 
 void loop() {
