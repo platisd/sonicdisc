a4931acbe8a7586fbba94883af74d5aaacfd574f
Dimitris Platis <kurukii88@gmail.com>
2017-08-07 12:28:31 +0300

    Add initial Sonic Sensor implementation

---
+++ hardware/firmware/SonicSensor.cpp
@@ -0,0 +1,26 @@
+#include "SonicSensor.h"
+
+
+SonicSensor::SonicSensor(uint8_t triggerPin, uint8_t echoPin) {
+    mTriggerPin = triggerPin;
+    mEchoPin = echoPin;
+    mTriggeredAt = 0;
+}
+
+uint8_t SonicSensor::getTriggerPin() {
+  return mTriggerPin;
+}
+
+uint8_t SonicSensor::getEchoPin() {
+  return mEchoPin;
+}
+
+unsigned long SonicSensor::setTriggerTime(unsigned long microseconds) {
+    unsigned long previousTrigger = mTriggeredAt;
+    mTriggeredAt = microseconds;
+    return previousTrigger;
+}
+
+unsigned long SonicSensor::getTriggerTime() {
+    return mTriggeredAt;
+}
--- hardware/firmware/SonicSensor.h
+++ hardware/firmware/SonicSensor.h
@@ -3,8 +3,40 @@
 
 class SonicSensor {
 public:
-  SonicSensor();
-  
+    /**
+     * The default constructor of a SonicSensor (HC-SR04 Ultrasonic sensor)
+     * @param triggerPin The trigger pin of the ultrasonic sensor
+     * @param echoPin    The echo pin of the ultrasonic sensor
+     */
+    SonicSensor(uint8_t triggerPin, uint8_t echoPin);
+
+    /**
+     * Gets the trigger pin of the ultrasonic sensor
+     * @return The trigger pin of the ultrasonic sensor
+     */
+    uint8_t getTriggerPin();
+
+    /**
+     * Gets the echo pin of the ultrasonic sensor
+     * @return The echo pin of the ultrasonic sensor
+     */
+    uint8_t getEchoPin();
+
+    /**
+     * Sets the trigger time of the ultrasonic sensor
+     * @param  microseconds The time (in microseconds) the sensor was triggered
+     * @return              The already existing trigger time
+     */
+    unsigned long setTriggerTime(unsigned long microseconds);
+
+    /**
+     * Gets the time that this sensor was triggered
+     * @return the time (in microseconds) that this sensor was triggered
+     */
+    unsigned long getTriggerTime();
+private:
+    uint8_t mTriggerPin, mEchoPin;
+    unsigned long mTriggeredAt;
 };
 
 #endif
